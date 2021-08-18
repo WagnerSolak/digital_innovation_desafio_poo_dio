0ee3ec6dab880b73571332968bc42442dfd0314b
= <=>
2021-08-18 17:32:07 -0300

    Finalizado Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -31,12 +29,45 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso);
+        /*System.out.println(curso);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
         //rodar Ctrl + Shift + F10
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devSolak = new Dev();
+        devSolak.setNome("Solak´s");
+        devSolak.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de: Solak" + devSolak.getConteudosInscritos());
+
+        devSolak.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de: Solak" + devSolak.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos de: Solak" + devSolak.getConteudosConcluidos());
+
+        devSolak.progredir();
+        System.out.println("XP: " + devSolak.calcularTotalXp());
+
+        System.out.println("----------------------------------------------------------------------------------------------------------");
+
+        Dev devJukis = new Dev();
+        devJukis.setNome("Jukis");
+        devJukis.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de: Jukis" + devSolak.getConteudosInscritos());
+        devJukis.progredir();
+        devJukis.progredir();
+        devJukis.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de: Jukis" + devJukis.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos de: Jukis" + devJukis.getConteudosConcluidos());
+        System.out.println("XP: " + devJukis.calcularTotalXp());
     }
 
 }
