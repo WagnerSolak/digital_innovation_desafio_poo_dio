a32e06a9f0fe4a624b55a25c76698bd0e2285677
= <=>
2021-08-18 16:42:58 -0300

    Refatorado Mentoria e Curso com conceito de heranca

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -16,19 +17,18 @@ public class Main {
         curso.setDescricao("descricao curso java");
         curso.setCargaHoraria(8);
 
-        System.out.println(curso);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
         curso2.setDescricao("descricao curso js");
         curso2.setCargaHoraria(8);
 
+        //Conteudo conteudo = new Curso(); // conceito de polimorfismo
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria java");
-
-        LocalDate hoje  = LocalDate.now();
-        mentoria.setData(hoje);
+        mentoria.setData(LocalDate.now());
 
 
         System.out.println(curso);
